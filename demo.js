eb23330bda040f22a97bc9716e904361e23870a8
perna saidev <saidev.marolix@gmail.com>
2024-09-05 23:05:15 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,20 @@
+console.log("welcome to javascript");
+// console is predefined object
+// log is the pre defined function
+console.log(10+"10") //concatanation happeened 1010
+console.log(100+ +"100") // adding extra + will convert string to number
+console.log(10+ +"10"+10) //concat //101010
+console.log(10- "2") // 8 interpreter will convert string to number by default
+console.log(10-2-8)    //0
+console.log(10+"2"-2)  //100 left to right execution
+console.log(10>9>8)   // 1>8 //false
+console.log(10*"10")  //100
+
+//== value
+//value and datatype
+
+console.log(1=="1")
+console.log(1+true)
+console.log(1==="1")
+
+
